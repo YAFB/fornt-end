ec1a5e43768d382e83ae9e4a1c5a58e9fe398f2a
YAFB <49499670+YAFB@users.noreply.github.com>
2020-04-08 10:57:22 -0600

    2

--- src/app/pages/descripcionservicio/descripcionservicio.page.ts
+++ src/app/pages/descripcionservicio/descripcionservicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-descripcionservicio',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescripcionservicioPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) { }
 
   ngOnInit() {
+  }
+  toggleMenu(){
+    this.menuCtrl.toggle();
   }
 
 }
